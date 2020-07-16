4fdca6830386473dae326cffb54e58c153b63494
getose <getose@outlook.com>
2020-07-16 21:15:38 +0200

    arg structure modification, arrays use : [[[x, y], [x, y]], ...]

--- src/lines-to-polygons.js
+++ src/lines-to-polygons.js
@@ -1,19 +1,20 @@
 /**
  *
- * @param [{{start : {x, y}, end : {x, y}}] lines
+ * @param [[[x, y], [x, y]], ...] lines
  */
 const linesToPolygons = (lines) => {
   const polygons = [];
   for (var i = 0; i < lines.length; i++) {
     // Récupération et suppression du tableau du premier élément
-    const currentLine = lines.splice(i--, 1)[0];
+    const firstLine = lines.splice(i--, 1)[0];
     // create a new polygon array
     const polygon = [];
+    // init current start point and current end point
+    let currentStartPoint = firstLine[0];
+    let currentEndPoint = firstLine[1];
     // put the 2 points of the first line on the polygon array
-    polygon.push(currentLine.start, currentLine.end);
-    // initi current start point and current end point
-    let currentstart = currentLine.start;
-    let currentend = currentLine.end;
+    polygon.push(currentStartPoint, currentEndPoint);
+   
     let j = 0;
     // init the linesLength
     let linesLength = lines.length;
@@ -29,54 +30,54 @@ const linesToPolygons = (lines) => {
       // check if the polygon is closed (the nextLine start point is one of the current start or end point and the nextLine end point is one of the current start or end point)
       if (
         polygon.length >= 3 &&
-        ((currentend.x === nextLine.start.x &&
-          currentend.y === nextLine.start.y &&
-          currentstart.x === nextLine.end.x &&
-          currentstart.y === nextLine.end.y) ||
-          (currentstart.x === nextLine.start.x &&
-            currentstart.y === nextLine.start.y &&
-            currentend.x === nextLine.end.x &&
-            currentend.y === nextLine.end.y))
+        ((currentEndPoint[0] === nextLine[0][0] &&
+          currentEndPoint[1] === nextLine[0][1] &&
+          currentStartPoint[0] === nextLine[1][0] &&
+          currentStartPoint[1] === nextLine[1][1]) ||
+          (currentStartPoint[0] === nextLine[0][0] &&
+            currentStartPoint[1] === nextLine[0][1] &&
+            currentEndPoint[0] === nextLine[1][0] &&
+            currentEndPoint[1] === nextLine[1][1]))
       ) {
         polygons.push(polygon);
         break;
       }
       if (
-        currentend.x === nextLine.start.x &&
-        currentend.y === nextLine.start.y
+        currentEndPoint[0] === nextLine[0][0] &&
+        currentEndPoint[1] === nextLine[0][1]
       ) {
         // end point of the current line equals to start point of the next line
-        polygon.push(nextLine.end);
+        polygon.push(nextLine[1]);
         // update current end point
-        currentend = nextLine.end;
+        currentEndPoint = nextLine[1];
         // Suppression de la ligne dans le tableau
         lines.splice(--j, 1);
       } else if (
-        currentstart.x === nextLine.start.x &&
-        currentstart.y === nextLine.start.y
+        currentStartPoint[0] === nextLine[0][0] &&
+        currentStartPoint[1] === nextLine[0][1]
       ) {
         // start point of the current line equals to start point of the next line
-        polygon.unshift(nextLine.end);
+        polygon.unshift(nextLine[1]);
         // update current start point
-        currentstart = nextLine.end;
+        currentStartPoint = nextLine[1];
         lines.splice(--j, 1);
       } else if (
-        currentend.x === nextLine.end.x &&
-        currentend.y === nextLine.end.y
+        currentEndPoint[0] === nextLine[1][0] &&
+        currentEndPoint[1] === nextLine[1][1]
       ) {
         // end point of the current line equals to end point of the next line
-        polygon.push(nextLine.start);
+        polygon.push(nextLine[0]);
         // update current end point
-        currentend = nextLine.start;
+        currentEndPoint = nextLine[0];
         lines.splice(--j, 1);
       } else if (
-        currentstart.x == nextLine.end.x &&
-        currentstart.y == nextLine.end.y
+        currentStartPoint[0] == nextLine[1][0] &&
+        currentStartPoint[1] == nextLine[1][1]
       ) {
         // start point of the current line equals to end point of the next line
-        polygon.unshift(nextLine.start);
+        polygon.unshift(nextLine[0]);
         // update current start point
-        currentstart = nextLine.start;
+        currentStartPoint = nextLine[0];
         lines.splice(--j, 1);
       }
     }
--- test/lines-to-polygons-test.js
+++ test/lines-to-polygons-test.js
@@ -3,26 +3,26 @@ var expect = chai.expect;
 var linesToPolygons = require("../lib/lines-to-polygons").default;
 
 describe("linesToPolygons", function () {
-  var point00 = { x: 0, y: 0 };
-  var point10 = { x: 1, y: 0 };
-  var point11 = { x: 1, y: 1 };
-  var point01 = { x: 0, y: 1 };
-  var point30 = { x: 3, y: 0 };
-  var point40 = { x: 4, y: 0 };
-  var point41 = { x: 4, y: 1 };
-  var point31 = { x: 3, y: 1 };
-  var point62 = { x: 6, y: 1 };
-  var point64 = { x: 6, y: 4 };
-  var line1 = { start: point00, end: point10 };
-  var line2 = { start: point10, end: point11 };
-  var line3 = { start: point11, end: point01 };
-  var line4 = { start: point01, end: point00 };
-  var line4i = { start: point00, end: point01 };
-  var line5 = { start: point30, end: point40 };
-  var line6 = { start: point40, end: point41 };
-  var line7 = { start: point41, end: point31 };
-  var line8 = { start: point31, end: point30 };
-  var line9 = { start: point62, end: point64 };
+  var point00 = [0, 0];
+  var point10 = [1, 0];
+  var point11 = [1, 1];
+  var point01 = [0, 1];
+  var point30 = [3, 0];
+  var point40 = [4, 0];
+  var point41 = [4, 1];
+  var point31 = [3, 1];
+  var point62 = [6, 1];
+  var point64 = [6, 4];
+  var line1 = [point00, point10];
+  var line2 = [point10, point11];
+  var line3 = [point11, point01];
+  var line4 = [point01, point00];
+  var line4i = [point00, point01];
+  var line5 = [point30, point40];
+  var line6 = [point40, point41];
+  var line7 = [point41, point31];
+  var line8 = [point31, point30];
+  var line9 = [point62, point64];
 
   it("linesToPolygons should return an empty array if an empty array is passed in", function () {
     expect(linesToPolygons([])).to.be.an("array").that.is.empty;
